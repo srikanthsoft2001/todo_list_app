d019d61855f50d8391f4ffb852fa202f01429c3e
srikanth <rmsrikanth25.gmail.com>
2025-02-15 22:19:22 +0530

    added the features like task and finish and delete

--- script.js
+++ script.js
@@ -11,17 +11,45 @@ function add_task() {
     alert("Enter the task and press the button!");
   } else {
     // creating the li element for each click of the add task button with the task text.
+    input_value.value = input_value.value.trim();
     const task = document.createElement("li");
-
-    task.innerHTML = document.querySelector(".input-task").value;
+    const task_text = document.createElement("span");
+    task_text.style.display = "inline-block";
+    task.appendChild(task_text);
+    task_text.innerHTML = document.querySelector(".input-task").value;
 
     //   adding the li to the ul element.
     task_list.appendChild(task);
 
     //   unwanted spaces before and after the task text is trimmed out.
-    input_value.value.trim();
+    // adding checkbox
+    const finish_checkbox = document.createElement("input");
+    finish_checkbox.type = "checkbox";
+    finish_checkbox.value = "checked";
+    task.appendChild(finish_checkbox);
 
+    // when the check box is create it creates the event listner and add to the checkbox , even when in function is inside the funcition it can execute.
+    finish_checkbox.addEventListener("change", function () {
+      if (finish_checkbox.checked) {
+        task_text.style.textDecoration = "line-through";
+      } else {
+        task_text.style.textDecoration = "none";
+      }
+    });
+
+    // adding button
+    const btn_delete = document.createElement("button");
+    btn_delete.innerHTML = "Delete Task";
+    task.appendChild(btn_delete);
+    // btn_delete.onclick = function () {
+    //   deleteTask(this);
+    // };
+    btn_delete.addEventListener("click", function () {
+      let listItem = btn_delete.parentElement;
+      listItem.remove();
+    });
     //   after pressing the button the input box must become empty.
     input_value.value = "";
   }
 }
+
